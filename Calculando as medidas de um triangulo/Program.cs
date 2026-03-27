d777212eeee65bcc899e68472478c57f28351339
henrique-pereira-pontes <henriqueppontes@hotmail.com>
2026-03-26 21:51:01 -0300

    Programa para calcular as medidas de um triângulo

---
+++ Calculando as medidas de um triangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿//Resolvendo o problema sem orientação à objetos
+
+using System;
+
+double xA;
+double xB;
+double xC;
+
+double yA;
+double yB;
+double yC;
+
+Console.WriteLine("Entre com as medidas do triângulo X:");
+    xA = double.Parse(Console.ReadLine());
+    xB = double.Parse(Console.ReadLine());
+    xC = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Entre com as medidas do triângulo Y:");
+    yA = double.Parse(Console.ReadLine());
+    yB = double.Parse(Console.ReadLine());
+    yC = double.Parse(Console.ReadLine());
+
+    double p = (xA + xB + xC) / 2;
+    double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+
+    p = (yA + yB + yC) / 2;
+    double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+ Console.WriteLine("Área de X = " + areaX);
+ Console.WriteLine("Área de Y = " + areaY);
+
+if (areaX > areaY)
+{
+    Console.WriteLine("A área X é maior");
+}
+else
+{
+    Console.WriteLine("A área Y é maior");
+
+}
