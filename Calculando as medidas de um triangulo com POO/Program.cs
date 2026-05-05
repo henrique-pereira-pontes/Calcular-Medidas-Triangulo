22532d96838404e22af69b20fb37ad048b870d47
henrique-pereira-pontes <henriqueppontes@hotmail.com>
2026-05-05 15:00:51 -0300

    Atualização: criei um método para calcular a área do triângulo dentro da propria classe.

---
+++ Calculando as medidas de um triangulo com POO/Program.cs
@@ -0,0 +1,39 @@
+﻿//Resolvendo o problema com orientação à objetos
+
+using Calculando_as_medidas_de_um_triangulo_com_POO;
+using System;
+
+Triangulo x, y;
+
+x = new Triangulo();
+y = new Triangulo();
+
+
+Console.WriteLine("Entre com as medidas do triângulo X:");
+    x.A = double.Parse(Console.ReadLine());
+    x.B = double.Parse(Console.ReadLine());
+    x.C = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Entre com as medidas do triângulo Y:");
+    y.A = double.Parse(Console.ReadLine());
+    y.B = double.Parse(Console.ReadLine());
+    y.C = double.Parse(Console.ReadLine());
+
+
+double areaX = x.Area();
+
+double areaY = y.Area(); 
+
+ Console.WriteLine("Área de X = " + areaX);
+ Console.WriteLine("Área de Y = " + areaY);
+
+if (areaX > areaY)
+{
+    Console.WriteLine("A área X é maior");
+}
+else
+{
+    Console.WriteLine("A área Y é maior");
+
+} 
---
+++ Calculando as medidas de um triangulo com POO/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculando_as_medidas_de_um_triangulo_com_POO
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        //Método para calcular a área do triângulo
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+
+    }
+}
